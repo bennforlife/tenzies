8eefa9f6cd2ef26920e4c28217571d8323eda79e
bennforlife <bennforlife@users.noreply.github.com>
2023-07-26 21:29:44 -0400

    Install & configure prettier and Airbnb styleguide

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,16 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: { react: { version: '18.2' }, impo },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
